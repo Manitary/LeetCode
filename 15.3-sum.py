43e9f88e258cfbf079adbf253887f678d66d8edd
Manitary <pak.ska@gmail.com>
2023-09-11 06:58:48 +0100

    Improve exercise 15
    Leave early if the lowest number is positive
    Remove checks that are more expensive than the operation

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -11,6 +11,8 @@ class Solution:
         ans: list[list[int]] = []
         nums = sorted(nums)
         for i, x in enumerate(nums):
+            if x > 0:
+                break
             if i > 0 and x == nums[i - 1]:
                 continue
             j = i + 1
@@ -19,19 +21,14 @@ class Solution:
                 n = x + nums[j] + nums[k]
                 if n < 0:
                     j += 1
-                    while j < k - 1 and nums[j] == nums[j - 1]:
-                        j += 1
                 elif n > 0:
                     k -= 1
-                    while j + 1 < k and nums[k] == nums[k + 1]:
-                        k -= 1
                 else:
-                    ans.append([x, nums[j], nums[k]])
+                    new = [x, nums[j], nums[k]]
+                    if not ans or ans[-1] != new:
+                        ans.append(new)
                     j += 1
                     k -= 1
-                    while j < k and nums[j] == nums[j - 1] and nums[k] == nums[k + 1]:
-                        j += 1
-                        k -= 1
 
         return ans
 
