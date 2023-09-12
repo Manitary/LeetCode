b0f58b5f58a0fbf419b95c5041b99f932da085f4
Manitary <pak.ska@gmail.com>
2023-09-12 20:32:55 +0100

    Improve exercise 23
    Use a heap instead
    Comparable runtime, simpler code

--- 23.merge-k-sorted-lists.py
+++ 23.merge-k-sorted-lists.py
@@ -6,33 +6,13 @@
 
 # @lc code=start
 # from typing import Self
+import heapq
 
-
-# # Definition for singly-linked list.
+# Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val: int = 0, next: Self | None = None) -> None:
 #         self.val = val
 #         self.next = next
-
-
-def mergeTwoLists(list1: ListNode | None, list2: ListNode | None) -> ListNode | None:
-    dummy_head = ListNode()
-    curr = dummy_head
-    while list1 or list2:
-        if not list1:
-            curr.next = list2
-            break
-        if not list2:
-            curr.next = list1
-            break
-        if list1.val <= list2.val:
-            curr.next = list1
-            list1 = list1.next
-        else:
-            curr.next = list2
-            list2 = list2.next
-        curr = curr.next
-    return dummy_head.next
 
 
 class Solution:
@@ -40,12 +20,17 @@ class Solution:
         # lists = list(filter(None, lists))
         if not lists:
             return
-        while len(lists) > 1:
-            lists = tuple(
-                mergeTwoLists(lists[i], lists[i + 1] if i + 1 < len(lists) else None)
-                for i in range(0, len(lists), 2)
-            )
-        return lists[0]
+        h: list[int] = []
+        for head in lists:
+            while head:
+                heapq.heappush(h, head.val)
+                head = head.next
+        ans = ListNode()
+        curr = ans
+        while h:
+            curr.next = ListNode(heapq.heappop(h))
+            curr = curr.next
+        return ans.next
 
 
 # @lc code=end
