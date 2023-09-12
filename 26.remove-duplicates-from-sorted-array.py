8182de85ae677a01f14529f55c84150a58fdb769
Manitary <pak.ska@gmail.com>
2023-09-12 21:03:43 +0100

    Update exercise 26
    Replace values instead of popping
    Memory tradeoff: create a set

--- 26.remove-duplicates-from-sorted-array.py
+++ 26.remove-duplicates-from-sorted-array.py
@@ -8,13 +8,10 @@
 # @lc code=start
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        i = 1
-        while i < len(nums):
-            if nums[i] == nums[i - 1]:
-                nums.pop(i)
-            else:
-                i += 1
-        return len(nums)
+        s = set(nums)
+        for i, x in enumerate(sorted(list(s))):
+            nums[i] = x
+        return len(s)
 
 
 # @lc code=end
