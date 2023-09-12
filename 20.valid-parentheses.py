ac42c1934cd8f6a03c7a19c46aee40c064e01b52
Manitary <pak.ska@gmail.com>
2023-09-12 17:02:07 +0100

    Solve: 20 - Valid Parentheses

---
+++ 20.valid-parentheses.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+
+
+# @lc code=start
+CLOSE = {")": "(", "]": "[", "}": "{"}
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        active: list[str] = []
+        for c in s:
+            if c not in CLOSE:
+                active.append(c)
+                continue
+            if not active or active[-1] != CLOSE[c]:
+                return False
+            active.pop()
+        return not active
+
+
+# @lc code=end
