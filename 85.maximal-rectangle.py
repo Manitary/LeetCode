bceeb4176103853ed5d6876bc692af4df3a804a2
Manitary <pak.ska@gmail.com>
2023-09-18 17:59:28 +0100

    Improve exercise 85
    Change algorithm entirely
    Add comments to explain

--- 85.maximal-rectangle.py
+++ 85.maximal-rectangle.py
@@ -8,21 +8,25 @@
 # @lc code=start
 class Solution:
     def maximalRectangle(self, matrix: list[list[str]]) -> int:
-        ans = 0
+        best = 0
         n_col = len(matrix[0])
-        for i in range(len(matrix)):
-            rect: list[float] = [0] * n_col
-            for row in matrix[i:]:
-                for j, x in enumerate(row):
-                    rect[j] += 1 if x == "1" else -float("inf")
-                max_so_far = -float("inf")
-                max_ending_here = 0
-                for x in rect:
-                    max_ending_here += x
-                    max_so_far = max(max_so_far, max_ending_here)
-                    max_ending_here = max(max_ending_here, 0)
-                ans = max(ans, max_so_far)
-        return ans
+        heights = [0] * (n_col + 1)
+        # add an extra fixed 0 at the end for a static reference point
+        for row in matrix:
+            for c, x in enumerate(row):
+                heights[c] = (heights[c] + 1) if x == "1" else 0
+                # max height if the cell is taken, looking upwards
+            height_stack = [-1]
+            for i in range(n_col + 1):
+                while heights[i] < heights[height_stack[-1]]:
+                    # a bump in height denote we passed a (local) maximum rectangle
+                    # the extra 0 at the end of heights guarantees we check the tail
+                    h = heights[height_stack.pop()]
+                    w = i - height_stack[-1] - 1
+                    best = max(best, h * w)
+                height_stack.append(i)
+
+        return best
 
 
 # @lc code=end
