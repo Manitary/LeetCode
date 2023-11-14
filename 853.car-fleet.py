d55ca90b859d0ceacc21f3895767182c7e861493
Manitary <pak.ska@gmail.com>
2023-11-14 12:58:36 +0000

    Improve 853
    Use a stack of arrival times to count fleets
    instead of popping and updating the car stack

--- 853.car-fleet.py
+++ 853.car-fleet.py
@@ -7,36 +7,19 @@
 
 # @lc code=start
 
-from typing import NamedTuple
-
-Car = NamedTuple("Car", [("position", int), ("speed", int)])
-
-
-def get_fleet(cars: list[Car], target: int) -> None:
-    car = cars.pop()
-    if not cars:
-        return
-    next_car = cars[-1]
-    if next_car.speed <= car.speed:
-        return
-    catch_up_time = (car.position - next_car.position) / (next_car.speed - car.speed)
-    catch_up_position = car.position + (car.speed) * catch_up_time
-    if catch_up_position > target:
-        return
-    cars[-1] = car
-    get_fleet(cars, target)
-
 
 class Solution:
     def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
-        cars = [Car(*x) for x in sorted(zip(position, speed))]
-        ans = 0
-
-        while cars:
-            get_fleet(cars, target)
-            ans += 1
-
-        return ans
+        cars = sorted(zip(position, speed), reverse=True)
+        arrival_time_stack: list[float] = []
+        for car_position, car_speed in cars:
+            arrival_time_stack.append((target - car_position) / car_speed)
+            if (
+                len(arrival_time_stack) >= 2
+                and arrival_time_stack[-1] <= arrival_time_stack[-2]
+            ):
+                arrival_time_stack.pop()
+        return len(arrival_time_stack)
 
 
 # @lc code=end
