13e1460f513037b7515009e8c061e3fcffebea7d
Manitary <pak.ska@gmail.com>
2023-11-14 16:49:40 +0000

    Improve 424
    Keep track of the amount of the most common character
    Better sliding window: slide right by 1 when going under the local maximum length
    Instead of doing max over the alphabet, only check the newest char
    Cache converting characters to array indices

--- 424.longest-repeating-character-replacement.py
+++ 424.longest-repeating-character-replacement.py
@@ -6,19 +6,29 @@
 
 
 # @lc code=start
+
+
+from functools import cache
+
+
+@cache
+def num(c: str) -> int:
+    return ord(c) - ord("A")
+
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        ans = 0
         left = 0
         charset = [0] * 26
-        for i, c in enumerate(s):
-            charset[ord(c) - ord("A")] += 1
-            while i - left + 1 - max(charset) > k:
-                charset[ord(s[left]) - ord("A")] -= 1
+        most_common = 0
+        for right, c in enumerate(s):
+            charset[num(c)] += 1
+            most_common = max(most_common, charset[num(c)])
+            if right - left + 1 - most_common > k:
+                charset[num(s[left])] -= 1
                 left += 1
-            ans = max(ans, i - left + 1)
 
-        return ans
+        return len(s) - left
 
 
 # @lc code=end
