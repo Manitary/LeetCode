39109c4df335335c384fd4250b98a20430cac310
Manitary <pak.ska@gmail.com>
2023-11-14 10:29:40 +0000

    Improve 49
    Use array based on alphabet characters to match anagrams

--- 49.group-anagrams.py
+++ 49.group-anagrams.py
@@ -6,20 +6,18 @@
 
 
 # @lc code=start
+from collections import defaultdict
+
+
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        anagrams: list[list[str]] = []
-        ans: list[list[str]] = []
+        ans: dict[tuple[int, ...], list[str]] = defaultdict(list)
         for s in strs:
-            a = sorted(s)
-            try:
-                i = anagrams.index(a)
-            except ValueError:
-                anagrams.append(a)
-                ans.append([s])
-            else:
-                ans[i].append(s)
-        return ans
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return list(ans.values())
 
 
 # @lc code=end
