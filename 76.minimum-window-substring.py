426d7e3baaeae4918856cb258db05c1b0591e5f5
Manitary <pak.ska@gmail.com>
2023-11-14 21:25:14 +0000

    Improve 76
    Replace ``is_contained`` with a faster check that the word is contained

--- 76.minimum-window-substring.py
+++ 76.minimum-window-substring.py
@@ -6,52 +6,45 @@
 
 
 # @lc code=start
-from functools import cache
-
-
-@cache
-def num(c: str) -> int:
-    x = ord(c) - ord("A")
-    if x > 25:
-        return x - 6
-    return x
-
-
-def is_contained(word: list[int], container: list[int]) -> bool:
-    for n1, n2 in zip(word, container):
-        if n1 > n2:
-            return False
-    return True
+from collections import Counter
 
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(t) > len(s):
             return ""
-        original = [0] * 52
-        for c in t:
-            original[num(c)] += 1
-        left, right = 0, len(t) - 1
+        conditions = Counter(t)
+        num_conditions = len(conditions)
+        num_satisfied = 0
+        satisfied = {x: False for x in conditions}
+
+        left = 0
         best = len(s) + 1
         shortest = None
-        charset = [0] * 52
-        for i, c in enumerate(s):
-            if i == len(t):
-                break
-            charset[num(c)] += 1
-        while left <= right < len(s):
-            if not is_contained(original, charset):
-                right += 1
-                if right < len(s):
-                    charset[num(s[right])] += 1
+        charset = {x: 0 for x in conditions}
+
+        for right, c in enumerate(s):
+            if c not in charset:
                 continue
-            while is_contained(original, charset):
-                new = right - left - 1
-                if new < best:
-                    best = new
+            charset[c] += 1
+            if not satisfied[c]:
+                satisfied[c] = charset[c] == conditions[c]
+                if satisfied[c]:
+                    num_satisfied += 1
+            while num_satisfied == num_conditions:
+                curr = right - left + 1
+                if curr < best:
+                    best = curr
                     shortest = (left, right)
-                charset[num(s[left])] -= 1
+                left_char = s[left]
                 left += 1
+                if left_char not in charset:
+                    continue
+                charset[left_char] -= 1
+                if satisfied[left_char]:
+                    satisfied[left_char] = charset[left_char] >= conditions[left_char]
+                    if not satisfied[left_char]:
+                        num_satisfied -= 1
 
         if not shortest:
             return ""
