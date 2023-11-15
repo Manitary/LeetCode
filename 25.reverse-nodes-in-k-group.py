2879c50bf71717a288547b6e7ed0292578628527
Manitary <pak.ska@gmail.com>
2023-11-15 17:00:29 +0000

    Improve 25
    Constant space solution

--- 25.reverse-nodes-in-k-group.py
+++ 25.reverse-nodes-in-k-group.py
@@ -5,10 +5,10 @@
 #
 
 # @lc code=start
+# # Definition for singly-linked list.
 # from typing import Self
 
 
-# # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val: int = 0, next: Self | None = None) -> None:
 #         self.val = val
@@ -17,27 +17,30 @@
 
 class Solution:
     def reverseKGroup(self, head: ListNode | None, k: int) -> ListNode | None:
-        if not head:
-            return
-        ans = ListNode()
-        curr = ans
-        while head:
-            batch: list[ListNode] = []
+        dummy_head = ListNode(next=head)
+        batch_left = dummy_head
+
+        while True:
+            curr = batch_left
             for _ in range(k):
-                if not head:
+                if not curr:
                     break
-                batch.append(head)
-                head = head.next
-            if len(batch) < k:
-                curr.next = batch[0]
-                while curr.next:
-                    curr = curr.next
-                break
-            for n in batch[::-1]:
-                curr.next = n
                 curr = curr.next
-        curr.next = None
-        return ans.next
+            if not curr:
+                break
+            k_th = curr
+            batch_right = k_th.next
+
+            prev, curr = batch_right, batch_left.next
+            while curr != batch_right:
+                assert curr
+                curr.next, prev, curr = prev, curr, curr.next
+
+            tmp = batch_left.next
+            batch_left.next = k_th
+            batch_left = tmp
+
+        return dummy_head.next
 
 
 # @lc code=end
