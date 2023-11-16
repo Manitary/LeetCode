c3abd1a434059bd3f716805f60bcc1806c4cea1b
Manitary <pak.ska@gmail.com>
2023-11-16 08:29:39 +0000

    Solve: 56 - Merge intervals

---
+++ 56.merge-intervals.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=56 lang=python3
+#
+# [56] Merge Intervals
+#
+
+
+# @lc code=start
+class Solution:
+    def insert(
+        self, intervals: list[list[int]], newInterval: list[int]
+    ) -> list[list[int]]:
+        ans: list[list[int]] = []
+        for i, old_interval in enumerate(intervals):
+            if newInterval[1] < old_interval[0]:
+                ans.append(newInterval)
+                ans.extend(intervals[i:])
+                return ans
+            if old_interval[1] < newInterval[0]:
+                ans.append(old_interval)
+                continue
+            newInterval = [
+                min(newInterval[0], old_interval[0]),
+                max(newInterval[1], old_interval[1]),
+            ]
+        ans.append(newInterval)
+
+        return ans
+
+    def merge(self, intervals: list[list[int]]) -> list[list[int]]:
+        ans: list[list[int]] = []
+        for interval in intervals:
+            ans = self.insert(ans, interval)
+        return ans
+
+
+# @lc code=end
