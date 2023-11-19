08c3d97f0d77b4c81d5486a9e9b88302becfce0d
Manitary <pak.ska@gmail.com>
2023-11-19 00:07:14 +0000

    Solve: 268 - Missing number

---
+++ 268.missing-number.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=268 lang=python3
+#
+# [268] Missing Number
+#
+
+
+# @lc code=start
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        return len(nums) * (len(nums) + 1) // 2 - sum(nums)
+
+
+# @lc code=end
